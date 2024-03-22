30b5557708ef34b01432d6d2b1ad144710279afa
lancer2672 <jbkhanhtran@gmail.com>
2024-03-22 14:08:55 +0700

    add env config

---
+++ utils/config.go
@@ -0,0 +1,25 @@
+package utils
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	GatewayAddress             string `mapstructure:"GATEWAY_ADDRESS"`
+	NotificationServiceAddress string `mapstructure:"NOTI_SERVICE_ADDR"`
+	MainServiceAddress         string `mapstructure:"MAIN_SERVICE_ADDR"`
+	MovieRESTAddress           string `mapstructure:"MOVIE_SERVICE_ADDR"`
+	MovieGRPCAddress           string `mapstructure:"MOVIE_SERVICE_GRPC_ADDR"`
+}
+
+// overrided by env if exists
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("dev")
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
